210d541adb26f3e9cb46fd6f7eed474fb74f1a8c
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2025-11-10 23:06:59 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-const base = "/dt193g-laboration1/";
-
 export default defineConfig({
-  base,
   build: {
     rollupOptions: {
       input: {
         home: resolve(__dirname, "index.html"),
         tailwind: resolve(__dirname, "tailwind/index.html"),
         bootstrap: resolve(__dirname, "bootstrap/index.html"),
+        publicering: resolve(__dirname, "publicering/index.html"),
       },
     },
   },
